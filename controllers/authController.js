0329aca768c9efa5656f548b775b23fc3a52b42f
emoalb <e_alb@abv.bg>
2022-10-31 22:07:04 +0200

    Signup and login....

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,18 @@
 const User = require("../models/userModel");
 
+const bcrypt = require("bcryptjs")
+
+
 exports.signUp = async (req,res) => {
+    const {username, password} = req.body;
+    
     try {
-        const newUser = await User.create(req.body);
+        const hashpassword = await bcrypt.hash(password,12);
+        const newUser = await User.create(
+           { username: username,
+            password: hashpassword
+           }
+        );
         res.status(201).json({
             status:'success',
             data:{
@@ -11,7 +21,39 @@ exports.signUp = async (req,res) => {
         });
     }catch(e) {
         res.status(400).json({
+            status:e,
+        });
+    };
+}
+exports.login = async(req,res)=>{
+    const {username, password} = req.body;
+    
+    try {
+       const user = await User.findOne({username})
+       if(!user){
+        res.status(404).json({
             status:"fail",
+            message:"User not found"
+        })
+       }
+     const isCorrect = await  bcrypt.compare(password,user.password)
+     if(isCorrect) {
+        res.status(200).json({
+            status:"success",
+            message:"Successfull login"
+        })
+     }else{
+        res.status(400).json({
+            status:"fail",
+            message:"Incorrect password"
+        })
+     }
+        
+
+    }catch(e) {
+        console.log(e);
+        res.status(400).json({
+            status:"Epic fail",
         });
     };
 }
